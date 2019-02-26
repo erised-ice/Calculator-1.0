9635e5912441ce97bd25d4117570e8f53d8ec247
alexandra <le.onze.mai@gmail.com>
2019-02-26 21:13:35 +0300

    changed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 function addition() {
-    var numberOne, numberTwo;
-    numberOne = document.getElementById('first-number').value;
-    numberTwo = document.getElementById('second-number').value;
+    var numberOne = document.getElementById('first-number').value;
+    numberOne = +numberOne;
+    var numberTwo = document.getElementById('second-number').value;
+    numberTwo = +numberTwo;
     result = numberOne + numberTwo;
     document.getElementById('result-number').innerHTML = result;
+    console.log(numberTwo);
+    console.log(numberOne);
     console.log(result);
+
 }
+
