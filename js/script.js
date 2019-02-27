3a372b9a32e210b29d9fc0f9a15d5834b294044c
alexandra <le.onze.mai@gmail.com>
2019-02-27 12:51:17 +0300

    working version with onclick

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
-function addition() {
-    var numberOne = document.getElementById('first-number').value;
-    numberOne = +numberOne;
-    var numberTwo = document.getElementById('second-number').value;
-    numberTwo = +numberTwo;
-    result = numberOne + numberTwo;
-    document.getElementById('result-number').innerHTML = result;
-    console.log(numberTwo);
-    console.log(numberOne);
-    console.log(result);
+var numberOne, numberTwo;
 
+function getNumber(id) {
+    return +document.getElementById(id).value;
 }
 
+function output(result) {
+    document.getElementById('display-result').value = result;
+}
+
+function addition() {
+    numberOne = getNumber('first-num');
+    numberTwo = getNumber('second-num');
+
+    output(numberOne + numberTwo);
+}
+
+function subtraction() {
+    numberOne = getNumber('first-num');
+    numberTwo = getNumber('second-num');
+
+    output(numberOne - numberTwo);
+}
+
+function multiplication() {
+    numberOne = getNumber('first-num');
+    numberTwo = getNumber('second-num');
+
+    output(numberOne * numberTwo);
+}
+
+function devision() {
+    numberOne = getNumber('first-num');
+    numberTwo = getNumber('second-num');
+
+    output(numberOne / numberTwo);
+}
+
+function deletion() {
+    document.getElementById('first-num').value = '';
+    document.getElementById('second-num').value = '';
+    output('');
+}
